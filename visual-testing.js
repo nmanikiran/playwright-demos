78576de3f9bda688c28fc72d10ddeb39da05b813
nmanikiran <nmanikiran15@gmail.com>
2020-12-25 17:37:33 +0530

    fixed visual testing

--- visual-testing.js
+++ visual-testing.js
@@ -3,14 +3,13 @@ const { chromium } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch({
-    _videosPath: __dirname, //  save videos here.
     headless: false,
   });
   const context = await browser.newContext({
-    _recordVideos: { width: 1024, height: 768 }, // downscale
+    videosPath: __dirname, //  save videos here.
+    videoSize: { width: 1024, height: 768 }, // downscale
   });
   const page = await context.newPage();
-  const video = await page.waitForEvent('_videostarted');
   await page.goto('https://gocovid19.netlify.app/');
   await page.waitForTimeout(1000);
   await page.click(`[href$="/WHO"]`);
@@ -27,6 +26,6 @@ const { chromium } = require('playwright');
   await page.waitForTimeout(1000);
   // ... perform actions
   await page.close();
-  fs.renameSync(await video.path(), 'video.webm');
+  fs.renameSync(await page.video().path(), 'visual-testing.webm');
   await browser.close();
 })();
